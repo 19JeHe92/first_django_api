28ccf6c70aac07d9ab4ce7ab4bc3c85630c2227a
Hempel <hempja@idmt.fraunhofer.de>
2017-12-12 17:25:30 +0100

    Create UserProfiles Viewset. Cinfigure urls.py file. Add user profile serializer.

--- src/webapp_project/profiles_api/views.py
+++ src/webapp_project/profiles_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
